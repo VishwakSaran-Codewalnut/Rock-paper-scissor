509fcfab6a6ca6dfa5e64b8926bf1e7cdf343669
VishwakSaran-Codewalnut <vishwak@codewalnut.com>
2025-12-27 14:41:13 +0530

    test: introduce Vitest setup with module mocks and custom render utility for React Testing Library.

--- test/setup.tsx
+++ test/setup.tsx
@@ -2,17 +2,15 @@
 import { vi } from 'vitest';
 import '@testing-library/jest-dom';
 
-// Mock Howl
 vi.mock('howler', () => ({
     Howl: vi.fn(() => ({
         play: vi.fn(),
     })),
 }));
 
-// Mock Next/Image
 vi.mock('next/image', () => ({
     default: ({ src, alt, ...props }: any) => (
         // eslint-disable-next-line @next/next/no-img-element
-        <img src= { src } alt={ alt } { ...props } />
-  ),
+        <img src={src} alt={alt} {...props} />
+    ),
 }));
--- test/test-utils.tsx
+++ test/test-utils.tsx
@@ -3,8 +3,6 @@ import { render, RenderOptions } from '@testing-library/react';
 import React, { ReactElement } from 'react';
 import GameContextProvider from '../src/app/context/gameContext';
 
-// This component wraps all tests in the necessary providers (Context, Theme, etc)
-// If you switch to Zustand/Redux, you ONLY edit this file.
 const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
     return (
         <GameContextProvider>
@@ -13,14 +11,10 @@ const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
     );
 };
 
-// Custom render function that includes providers
 const customRender = (
     ui: ReactElement,
     options?: Omit<RenderOptions, 'wrapper'>,
 ) => render(ui, { wrapper: AllTheProviders, ...options });
-
-// Re-export everything from testing-library
 export * from '@testing-library/react';
 
-// Override the render method
 export { customRender as render };
